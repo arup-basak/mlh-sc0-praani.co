7ad4b4f74cb0acdcc421d3b35d6e6a24073c1bca
Arup Basak <95271566+arup-basak@users.noreply.github.com>
2023-08-12 16:57:58 +0530

    Modified footer,modified footer,added landing page ui

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,11 +3,12 @@ import React from 'react'
 interface Props {
     text: string,
     onClick: () => void
+    className?: string
 }
 
 const Button = (props: Props) => {
     return (
-        <button onClick={props.onClick} className=''>
+        <button onClick={props.onClick} className={`${props.className} p-2 px-4`}>
             {props.text}
         </button>
     )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,50 @@
 import React from 'react'
+import Head from 'next/head'
 import Heading from '@/components/Heading'
+import Button from '@/components/Button'
 
 const Home = () => {
   return (
     <>
-      <Heading text='This is Heading' />
+      <Head>
+        <title>Welcome</title>
+      </Head>
+      <main>
+        <Heading
+          text='Revolutionizing healthcare one click at a time.'
+        />
+        <section>
+          <Heading
+            text='Emerge as a Victor'
+          />
+        </section>
+        <section>
+          <div className='text-2xl'>
+            Our emergency page provides real-time location tracking, so help is never far away. Filter hospitals by distance, bed availability, and more to find the perfect match.
+          </div>
+          <div className='text-2xl'>
+            Stay connected and never miss a beat with Hospital Connect. Schedule visits, access facilities, and manage appointments seamlessly.
+          </div>
+          <div className='text-2xl'>
+            Simplify your life with our pathology services! Book tests, retrieve results, and gain insights into your health.
+          </div>
+        </section>
+        <section>
+          <Heading
+            text='Ready to transform your healthcare experience? Join now!'
+          />
+          <div>
+            <Button
+              text='Sign Up'
+              onClick={() => { }}
+            />
+            <Button
+              text='Learn More'
+              onClick={() => { }}
+            />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
