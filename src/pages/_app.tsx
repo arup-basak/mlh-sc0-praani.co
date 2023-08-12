b32635aa920c37709f342d5d5494dfe9713383c1
Rajdip Sinha <rajdipsinha@Anirbans-MacBook-Pro.local>
2023-08-12 23:57:13 +0530

    Home page done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { UserProvider } from '@auth0/nextjs-auth0/client';
-import Footer from '@/components/Footer';
-import { Golos_Text } from 'next/font/google'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
+import Footer from "@/components/Footer";
+import { Golos_Text } from "next/font/google";
+import Navbar from "@/components/Navbar";
 
-const font = Golos_Text({ subsets: ['latin'], weight: ['400'] })
+const font = Golos_Text({ subsets: ["latin"], weight: ["400"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
       <main className={font.className}>
+        <Navbar></Navbar>
         <Component {...pageProps} />
         <Footer />
       </main>
     </UserProvider>
-  )
+  );
 }
