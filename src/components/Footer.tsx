7769da87200e1485e9006285587aac7fb2aa1cb0
Arup Basak <95271566+arup-basak@users.noreply.github.com>
2023-08-12 16:57:58 +0530

    Modified footer,modified footer,added landing page ui

---
+++ src/components/Footer.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import Link from 'next/link'
+
+interface ButtonProps { text: string, url: string }
+
+const Button = ({ text, url }: ButtonProps) => {
+    return (
+        <Link href={url} className='p-2'>
+            {text}
+        </Link>
+    )
+}
+
+const Footer = () => {
+    const buttons = [
+        { "text": "Services", "url": "/services" },
+        { "text": "Emergency", "url": "/emergency" },
+        { "text": "Hospital", "url": "/hospital" },
+        { "text": "Pathology", "url": "/pathology" },
+        { "text": "Support", "url": "/support" },
+        { "text": "FAQ", "url": "/faq" },
+        { "text": "Contact", "url": "/contact" },
+        { "text": "Pricing", "url": "/pricing" },
+        { "text": "Legal", "url": "/legal" },
+        { "text": "Privacy", "url": "/privacy" },
+        { "text": "Terms", "url": "/terms" },
+        { "text": "Disclaimer", "url": "/disclaimer" }
+    ]
+    return (
+        <section className='bg-black text-white '>
+            <div className='flex justify-center'>
+                <div className='w-5/12 grid grid-cols-3 p-2'>
+                    {
+                        buttons.map((item, index) => (
+                            <Button
+                                key={index}
+                                text={item.text}
+                                url={item.url}
+                            />
+                        ))
+                    }
+                </div>
+            </div>
+            {/* <div className='flex justify-center w-5/12'>
+                <div>
+                    © 2023 Emergency Hospital Connect. All rights reserved.
+                </div>
+                <div>
+
+                </div>
+            </div> */}
+        </section>
+    )
+}
+
+export default Footer;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { UserProvider } from '@auth0/nextjs-auth0/client';
+import Footer from '@/components/Footer';
+import { Golos_Text } from 'next/font/google'
+
+const font = Golos_Text({ subsets: ['latin'], weight: ['400'] })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
-      <Component {...pageProps} />
+      <main className={font.className}>
+        <Component {...pageProps} />
+        <Footer />
+      </main>
     </UserProvider>
   )
 }
