95659ce2015f28b9975dfb095bce875b32f91b69
Arup Basak <95271566+arup-basak@users.noreply.github.com>
2023-08-13 13:24:09 +0530

    added Coin for Incerment and Decrement

---
+++ src/pages/api/coin.ts
@@ -0,0 +1,27 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { model, models } from "mongoose";
+import mongo from "@/libs/mongo";
+import userSchema from "@/schemas/user.schema";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<any>
+) {
+    mongo();
+    const sid = req.body.sid as string;
+    const value = req.body.value as number;
+
+    try {
+        const mModel = models.users || model("users", userSchema);
+
+        const resp = await mModel.findOneAndUpdate(
+            { sid: sid },
+            { $inc: { coins: value } },
+            { new: true }
+        )
+        res.status(200).json({ response: true });
+    } catch (error: any) {
+        console.log(error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
