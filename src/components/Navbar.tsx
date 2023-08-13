e30383d9b712f2c69290ca062ca6a3123ff28a52
Arup Basak <95271566+arup-basak@users.noreply.github.com>
2023-08-13 12:29:40 +0530

    added Logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
 import { useUser } from '@auth0/nextjs-auth0/client';
-import { FaUser } from 'react-icons/fa';
+import { FaUser, FaCoins } from 'react-icons/fa';
 import { useEffect, useState } from "react";
 import axios from 'axios';
+import logo_img from 'public/logo.svg'
+import Image from "next/image";
 
 const Navbar = () => {
   const { user, error, isLoading } = useUser();
@@ -12,14 +14,13 @@ const Navbar = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        // console.log(user)
         const resp = await axios.post('/api/user', { ...user });
         setCoin(resp.data._doc.coins);
       } catch (error) {
         console.error("Error fetching user data:", error);
       }
     };
-    
+
     if (user && !sended && coin === 0) {
       fetchData();
       setSended(true);
@@ -31,14 +32,27 @@ const Navbar = () => {
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between py-4 md:py-6">
           <Link href="/" className="flex items-center text-white">
-              {/* <img src="" alt="Logo" className="h-8" /> */}
+            <Image
+              src={logo_img}
+              height={30}
+              width={30}
+              alt="Logo"
+              className="scale-150"
+            />
+            <span className="mx-3 font-semibold text-2xl">
+              LogoText
+            </span>
+            {/* <img src="" alt="Logo" className="h-8" /> */}
           </Link>
           <div className="hidden md:flex space-x-6 text-white">
-            <div>{coin}</div>
             <Link href="/" className="hover:text-yellow-500">Home</Link>
             <Link href="/products" className="hover:text-yellow-500">Products</Link>
             <Link href="/dashboard" className="hover:text-yellow-500">Dashboard</Link>
             <Link href="/about" className="hover:text-yellow-500">About us</Link>
+            <div className="flex items-center space-x-2">
+              <FaCoins className="text-white" />
+              <span className="text-white">{coin} Coins</span>
+            </div>
           </div>
           {user ? (
             <div className="text-white">
