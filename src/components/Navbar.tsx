58bc1e4a297c02d369cdf6351d26e2d125f04b57
Rajdip Sinha <rajdipsinha@Anirbans-MacBook-Pro.local>
2023-08-13 08:27:35 +0530

    About us page done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,46 +1,54 @@
 import Link from "next/link";
-import { useUser } from '@auth0/nextjs-auth0/client';
-import account_Icon from 'public/account.svg'
+import { useUser } from "@auth0/nextjs-auth0/client";
+import account_Icon from "public/account.svg";
 import Image from "next/image";
-import { FaUser } from 'react-icons/fa'
+import { FaUser } from "react-icons/fa";
 
 const Navbar = () => {
   const { user, error, isLoading } = useUser();
-  console.log(user)
+  console.log(user);
 
   return (
     <div className="sticky top-0 bg-black">
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="container px-4 mx-auto sm:px-6 lg:px-8">
         <div className="flex items-center justify-between py-4 md:py-6">
           <Link href="/">
             <div className="flex items-center text-white">
               <img src="" alt="Logo" className="h-8" />
             </div>
           </Link>
-          <div className="hidden md:flex space-x-6 text-white">
-            <Link href="/" className="hover:text-yellow-500">Home</Link>
-            <Link href="/products" className="hover:text-yellow-500">Products</Link>
-            <Link href="/dashboard" className="hover:text-yellow-500">Dashboard</Link>
+          <div className="hidden space-x-6 text-white md:flex">
+            <Link href="/" className="hover:text-yellow-500">
+              Home
+            </Link>
+            <Link href="/products" className="hover:text-yellow-500">
+              Products
+            </Link>
+            <Link href="/dashboard" className="hover:text-yellow-500">
+              Dashboard
+            </Link>
+            <Link href="/about" className="hover:text-yellow-500">
+              About Us
+            </Link>
           </div>
-          {
-            user ? (
-              <div className="text-white">
-                <FaUser />
-              </div>
-            ) : (
-              <button>
-                <a
-                  href="/api/auth/login"
-                  className="inline-block px-4 py-2 bg-yellow-500 text-black rounded-lg">
-                  Login
-                </a>
-              </button>
-            )
-          }
+          {user ? (
+            <div className="text-white">
+              <FaUser />
+            </div>
+          ) : (
+            <button>
+              <a
+                href="/api/auth/login"
+                className="inline-block px-4 py-2 text-black bg-yellow-500 rounded-lg"
+              >
+                Login
+              </a>
+            </button>
+          )}
         </div>
-      </div >
-    </div >
+      </div>
+    </div>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { motion } from "framer-motion";
 import Head from "next/head";
 import Heading from "@/components/Heading";
-import Image from 'next/image';
+import Image from "next/image";
 import bgImg from "public/dogBG.png";
-import catImg from 'public/cat.jpg';
-import dogImg from 'public/dog.jpg';
+import catImg from "public/cat.jpg";
+import dogImg from "public/dog.jpg";
 
 const Home = () => {
   return (
@@ -17,12 +17,12 @@ const Home = () => {
         <div
           style={{
             backgroundImage: `url(${bgImg.src})`,
-            width: '100%',
-            minHeight: '80vh',
-            backgroundSize: 'cover',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
+            width: "100%",
+            minHeight: "80vh",
+            backgroundSize: "cover",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
           }}
         >
           <motion.span
@@ -39,25 +39,33 @@ const Home = () => {
               opacity: 1,
             }}
           >
-            <Heading text="FURTASTIC" className="text-[3rem] sm:text-[5rem] md:text-[8rem] lg:text-[10rem]" />
+            <Heading
+              text="FURTASTIC"
+              className="text-[3rem] sm:text-[5rem] md:text-[8rem] lg:text-[10rem]"
+            />
           </motion.span>
         </div>
       </section>
       <section className="bg-[#fdfcdc]">
-        <div className="w-full lg:w-7/12 text-lg lg:text-xl py-5 px-4 lg:px-8">
+        <div className="w-full px-4 py-5 text-lg lg:w-7/12 lg:text-xl lg:px-8">
           <div className="py-2">
-            Welcome to PetMed, where your pets' health matters. Our services cater to cats, dogs, fish, and birds.
+            Welcome to PetMed, where your pets' health matters. Our services
+            cater to cats, dogs, fish, and birds.
           </div>
           <div className="py-2">
-            Got a pet emergency on your hands? Worry not! We've got emergency ambulance services and hospital bookings based on your live location.
+            Got a pet emergency on your hands? Worry not! We've got emergency
+            ambulance services and hospital bookings based on your live
+            location.
           </div>
           <div className="py-2">
-            Dive into our services including hospitals, pathology, and a dedicated store. Sign up and access your profile to make the most of PetMed.
+            Dive into our services including hospitals, pathology, and a
+            dedicated store. Sign up and access your profile to make the most of
+            PetMed.
           </div>
         </div>
       </section>
       <section className="bg-[#fdfcdc]">
-        <div className="grid grid-cols-1 lg:grid-cols-2 p-3">
+        <div className="grid grid-cols-1 p-3 lg:grid-cols-2">
           <div className="lg:mx-4 xl:mx-20">
             <Image
               src={dogImg}
@@ -68,19 +76,28 @@ const Home = () => {
               height={300}
             />
           </div>
-          <div className="py-5 px-4 lg:px-8">
-            <Heading text="Why Choose PetMed?" className="text-3xl pb-2" />
-            <Heading text="Your Pets Deserve the Best!" className="text-4xl pb-4" />
+          <div className="px-4 py-5 lg:px-8">
+            <Heading text="Why Choose PetMed?" className="pb-2 text-3xl" />
+            <Heading
+              text="Your Pets Deserve the Best!"
+              className="pb-4 text-4xl"
+            />
             <div className="text-base lg:text-lg">
-              Wave goodbye to pet health uncertainties! Our experienced vets, partnered healthcare companies, and glowing customer reviews will put your mind at ease.
+              Wave goodbye to pet health uncertainties! Our experienced vets,
+              partnered healthcare companies, and glowing customer reviews will
+              put your mind at ease.
             </div>
           </div>
         </div>
-        <div className="grid grid-cols-1 lg:grid-cols-2 p-3">
-          <div className="py-5 px-4 lg:px-8">
-            <Heading text="Got Questions? We’ve Got Answers!" className="text-4xl pb-2" />
+        <div className="grid grid-cols-1 p-3 lg:grid-cols-2">
+          <div className="px-4 py-5 lg:px-8">
+            <Heading
+              text="Got Questions? We’ve Got Answers!"
+              className="pb-2 text-4xl"
+            />
             <div className="text-base lg:text-lg">
-              Unleash your curiosity in our FAQs section, where we clear up your doubts and keep your pets happy and healthy.
+              Unleash your curiosity in our FAQs section, where we clear up your
+              doubts and keep your pets happy and healthy.
             </div>
           </div>
           <div className="lg:mx-4 xl:mx-20">
