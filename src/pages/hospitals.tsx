527d25b55cb30e5f5b1d30c7cee540d0438e9e01
Nikhil Mishra <108953684+mnik7044@users.noreply.github.com>
2023-08-13 13:44:03 +0530

    Did the hospital page

--- src/pages/hospitals.tsx
+++ src/pages/hospitals.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import LoadingSpinner from '@/components/LoadingSpinner';
 
 const Hospital = () => {
-  return (
-    <div>Welcome to Dashboard</div>
-  )
+    const [data, setData] = useState<any[]>()
+    const [isLoading, setLoading] = useState(true);
+    const location = [12, 13];
+    useEffect(() => {
+        const fetchData = async () => {
+            const resp = await axios.post('/api/hospitals', {
+                position: location
+            })
+            if(resp.data) {
+                const hospitals = resp.data
+                console.log(hospitals)
+                setData(hospitals)
+                setLoading(false)
+            }
+        }
+
+        if(isLoading) {
+            fetchData()
+       }
+    })
+    if(isLoading) {
+        return <LoadingSpinner />
+    }
+    return (
+        <div>
+            {
+                data?.map((item, index) => {
+                    return (
+                        <div key={index}>
+                            {item.name}
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
 }
 
 export default Hospital
